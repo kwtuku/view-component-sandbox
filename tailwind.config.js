c1ee49c480e00e523b828a2a15b499899fd74a83
kwtuku <76758720+kwtuku@users.noreply.github.com>
2023-06-03 23:25:54 +0900

    chore: view_component と他ライブラリとの設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
     "./app/assets/stylesheets/**/*.css",
+    "./app/components/**/*.{html.erb,rb}",
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js",
     "./app/views/**/*.html.erb",

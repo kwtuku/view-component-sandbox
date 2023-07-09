48c505f7bd27b490a73743106e4c98c11db2ac43
kwtuku <76758720+kwtuku@users.noreply.github.com>
2023-07-09 22:11:48 +0900

    feat: コンポーネントを出し分けられるように

---
+++ app/javascript/components/Hi.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+const Hi = () => <div>Hi React!</div>
+
+export default Hi
--- app/javascript/controllers/react_controller.js
+++ app/javascript/controllers/react_controller.js
@@ -1,14 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 import React, { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+
 import Hello from "../components/Hello"
+import Hi from "../components/Hi"
 
 export default class extends Controller {
+  static values = { name: String }
+
+  initialize() {
+    this.Components = {
+      Hello: Hello,
+      Hi: Hi,
+    }
+  }
+
   connect() {
+    const Component = this.Components[this.nameValue]
     const root = createRoot(this.element)
     root.render(
       <StrictMode>
-        <Hello />
+        <Component />
       </StrictMode>,
     )
   }

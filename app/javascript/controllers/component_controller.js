3d7a9f3f21b72de5251ebfbbc31b5afb7f613d3d
kwtuku <76758720+kwtuku@users.noreply.github.com>
2023-08-06 03:11:24 +0900

    refactor: Product Quickview を react に書き換え

--- app/javascript/controllers/component_controller.js
+++ app/javascript/controllers/component_controller.js
@@ -5,6 +5,7 @@ import { createRoot } from "react-dom/client"
 import Hello from "../components/Hello"
 import Hi from "../components/Hi"
 import CategoryFilter from "../components/CategoryFilter"
+import ProductQuickview from "../components/ProductQuickview"
 
 export default class extends Controller {
   static values = { name: String }
@@ -14,12 +15,14 @@ export default class extends Controller {
       Hello: Hello,
       Hi: Hi,
       CategoryFilter: CategoryFilter,
+      ProductQuickview: ProductQuickview,
     }
   }
 
   connect() {
     const root = createRoot(this.element)
     const Component = this.components[this.nameValue]
+    console.log(this.nameValue)
     root.render(
       <StrictMode>
         <Component />

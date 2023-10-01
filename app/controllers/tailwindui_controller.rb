91007c39e9e6d1d813f7822b41c760abf2507ccc
kwtuku <76758720+kwtuku@users.noreply.github.com>
2023-10-01 23:23:22 +0900

    feat: ナブバーがないレイアウトも使えるように

--- app/controllers/tailwindui_controller.rb
+++ app/controllers/tailwindui_controller.rb
@@ -1,5 +1,11 @@
 class TailwinduiController < ApplicationController
-  layout "tailwindui"
+  layout proc { |controller|
+    list = %w[
+      sign_in navbar stacked_layout store_navigation promo_section header_section newsletter_section hero_section content_section not_found contact_section
+      banner feature_section_with_product_screenshot blog_section header pricing_section feature_section_centered_2x2_grid
+    ]
+    controller.action_name.in?(list) ? "no_navbar" : "tailwindui"
+  }
 
   Tailwindui::Example.names.each do |name|
     define_method(name) do

bb1e79f0294f6b6cc23f61df3200cd7ab376df75
kwtuku <76758720+kwtuku@users.noreply.github.com>
2023-10-01 22:16:44 +0900

    test: react のコンポーネントが表示されることのテスト

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe "トップページ" do
 
     expect(page).to have_content "Hello, World!"
   end
+
+  it "React のコンポーネントが表示される" do
+    visit root_path
+
+    expect(page).to have_content "Hello React!"
+  end
 end

01c729b3c69622914a1e7bee3a68c8db77f9145d
kwtuku <76758720+kwtuku@users.noreply.github.com>
2023-10-09 01:21:03 +0900

    chore: コンポーネント内で devise のヘルパーを使っていてもテストできる設定

--- spec/support/view_component.rb
+++ spec/support/view_component.rb
@@ -6,4 +6,9 @@ RSpec.configure do |config|
   config.include ViewComponent::TestHelpers, type: :component
   config.include ViewComponent::SystemTestHelpers, type: :component
   config.include Capybara::RSpecMatchers, type: :component
+
+  config.include Devise::Test::ControllerHelpers, type: :component
+  config.before(:each, type: :component) do
+    @request = vc_test_controller.request
+  end
 end

c2b88b467d15e67fd3a5e0780d6d27f8d723504e
kwtuku <76758720+kwtuku@users.noreply.github.com>
2023-10-26 21:37:34 +0900

    fix: awesome_nested_set の #roots は順序が指定されている

--- app/controllers/nested_set_items_controller.rb
+++ app/controllers/nested_set_items_controller.rb
@@ -2,7 +2,7 @@ class NestedSetItemsController < ApplicationController
   layout "my_models"
 
   def index
-    @nested_set_items = NestedSetItem.then { params[:type] == "roots" ? _1.roots : _1 }.order(:created_at)
+    @nested_set_items = NestedSetItem.then { params[:type] == "roots" ? _1.roots : _1.order(:created_at) }
 
     render NestedSetItemTreeComponent.new(items: @nested_set_items, is_children: false) if params[:type] == "roots"
   end

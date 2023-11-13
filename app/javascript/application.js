880863738b00e9a1ae385ac543b5cb688b14c2ec
kwtuku <76758720+kwtuku@users.noreply.github.com>
2023-11-13 22:02:26 +0900

    chore: action cable のパッケージのバージョンを 7.0.8 に変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import "./channels"
 
 Turbo.setProgressBarDelay(0)
-import "./channels"
